fd1cc37f6f7019cf7ffc29ce563f147c498664d6
mvarelau <141885396+mvarelau@users.noreply.github.com>
2023-10-13 22:10:11 -0500

    8-6.py

--- 8-6.py
+++ 8-6.py
@@ -3,7 +3,7 @@ def potencias():
   n=int(input("Ingrese un npumero natural: "))
   x=float(input("Ingrese otro número natural: "))
   resultado = 1
-  for _ in range(n):
+  for i in range(1, n+1):
     resultado *= x
   print(f"El resultado de {x}^{n} es igual a {resultado}")
 
