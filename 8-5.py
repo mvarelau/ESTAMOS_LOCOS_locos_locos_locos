63e16082061110dd4f05b76a7d5e68fac469726f
mvarelau <141885396+mvarelau@users.noreply.github.com>
2023-10-13 22:09:03 -0500

    8-5.py

--- 8-5.py
+++ 8-5.py
@@ -1,14 +1,11 @@
 #8-5
 def calcular_potencia_dos(n):
     resultado = 1
-    for _ in range(n):
+    for i in range(1,n+1):
         resultado *= 2
     return resultado
 
 n = int(input("Ingrese un número entero para calcular 2^n: "))
-if n < 0:
-    print("El exponente debe ser un número entero no negativo.")
-else:
-    resultado = calcular_potencia_dos(n)
-    print(f"2^{n} es igual a {resultado}")
+resultado = calcular_potencia_dos(n)
+print(f"2^{n} es igual a {resultado}")
 
